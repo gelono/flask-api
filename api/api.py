ecf9ddb2c007c31fcd08bc4a0226359dd3e824e6
gelonooz <gelonooz@gmail.com>
2024-04-26 19:49:45 +0300

    Update ssl

--- api/api.py
+++ api/api.py
@@ -28,5 +28,5 @@ def hello_world():
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(host='https://vm5043127.43ssd.had.wf', port=5000, ssl_context=(r'C:\wacs\crt\vm5043127.43ssd.had.wf-crt.pem',
+    app.run(host='vm5043127.43ssd.had.wf', port=5000, ssl_context=(r'C:\wacs\crt\vm5043127.43ssd.had.wf-crt.pem',
                                                            r'C:\wacs\crt\vm5043127.43ssd.had.wf-key.pem'))
