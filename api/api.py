4b513345f500e8deb8bff6f6a1463c9c25067604
gelonooz <gelonooz@gmail.com>
2024-04-26 13:51:44 +0300

    first commit

---
+++ api/api.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+
+def check_token(token):
+    with open('../storage/tokens.txt', 'r') as file:
+        tokens = file.read().splitlines()
+        return token in tokens
+
+
+@app.route('/hello', methods=['POST'])
+def hello_world():
+    if request.method == 'POST':
+        auth_header = request.headers.get('Authorization')
+        if auth_header and auth_header.startswith('Bearer '):
+            token = auth_header.split(' ')[1]
+            if check_token(token):
+                return jsonify({"message": "Hello world"}), 200
+            else:
+                return jsonify({"error": "Invalid token"}), 401
+        else:
+            return jsonify({"error": "Missing or invalid Authorization header"}), 401
+    else:
+        return jsonify({"error": "Method not allowed"}), 405
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ check_api.py
@@ -0,0 +1,15 @@
+import requests
+
+
+url = 'http://127.0.0.1:5000/hello'
+# token = 'skdjgh14dfjbl246dklwjgqsdfvbsfb2r52'
+token = 'uaysdgfuqcyk13rkuahcvuy3115135'
+
+headers = {'Authorization': 'Bearer ' + token}
+response = requests.post(url, headers=headers)
+
+if response.status_code == 200:
+    print("Response:", response.json())
+else:
+    print("Error:", response.status_code)
+    print("Response:", response.json())
