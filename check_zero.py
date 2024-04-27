afadfb9b4d748c06a0671a97099d0954a9b8509e
gelonooz <gelonooz@gmail.com>
2024-04-27 15:49:32 +0300

    Update zero

--- check_zero.py
+++ check_zero.py
@@ -3,8 +3,10 @@ import zmq
 
 context = zmq.Context()
 socket = context.socket(zmq.REQ)  # REQ (REQUEST) socket for sending requests
-socket.connect("tcp://vm5043127.43ssd.had.wf:5000")
+# socket.connect("tcp://vm5043127.43ssd.had.wf:5000")
+socket.connect("tcp://localhost:5000")
 
-socket.send(b"Hello")
+# socket.send(b"Hello")
+socket.send(b"uaysdgfuqcyk13rkuahcvuy3115135")
 message = socket.recv()
 print(f"Received reply: {message.decode('utf-8')}")
