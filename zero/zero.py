b8e715e47b333cdd880c4e9ff2ab0e2c8ed2c5d6
gelonooz <gelonooz@gmail.com>
2024-04-29 08:54:56 +0300

    zero ssl

--- zero/zero.py
+++ zero/zero.py
@@ -1,30 +1,63 @@
+# import zmq
+# import logging
+#
+# # Открытый/закрытый ключи сервера
+# server_public_key = b'jA8U*z0y7M59cxCMtH)L[?ornj><R@MLe)V>T5FX'
+# server_secret_key = b'dPoKlIUK&xI9wH:+]E1mhv(GSna]i-EUyUs{$Q=4'
+#
+# # Инициализация системы логирования
+# logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+#
+# def check_token(token):
+#     with open('../storage/tokens.txt', 'r') as file:
+#         tokens = file.read().splitlines()
+#         return token in tokens
+#
+#
+# context = zmq.Context.instance()
+#
+# # Создаем серверный сокет и настраиваем безопасность
+# server_socket = context.socket(zmq.REP)
+# server_socket.curve_secretkey = server_secret_key
+# server_socket.curve_publickey = server_public_key
+# server_socket.curve_server = True
+# server_socket.bind("tcp://*:5000")
+#
+# while True:
+#     message = server_socket.recv()
+#     token = message.decode('utf-8')
+#
+#     # Запись входящего сообщения в журнал
+#     logging.info(f"Received request: {token}")
+#
+#     if check_token(token):
+#         print(f"Received request: {token}")
+#         server_socket.send(b"World")
+#     else:
+#         server_socket.send(b"Error token")
 import zmq
+import time
+import sys
+import ssl
 
-# Открытый/закрытый ключи сервера
-server_public_key = b'jA8U*z0y7M59cxCMtH)L[?ornj><R@MLe)V>T5FX'
-server_secret_key = b'dPoKlIUK&xI9wH:+]E1mhv(GSna]i-EUyUs{$Q=4'
+port = "5000"
+if len(sys.argv) > 1:
+    port = sys.argv[1]
+    int(port)
 
+# Создаем серверный сокет с типом "zmq.REP" и привязываем его к известному порту
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:%s" % port)
 
-def check_token(token):
-    with open('../storage/tokens.txt', 'r') as file:
-        tokens = file.read().splitlines()
-        return token in tokens
-
-
-context = zmq.Context.instance()
-
-# Создаем серверный сокет и настраиваем безопасность
-server_socket = context.socket(zmq.REP)
-server_socket.curve_secretkey = server_secret_key
-server_socket.curve_publickey = server_public_key
-server_socket.curve_server = True
-server_socket.bind("tcp://*:5000")
+# Настройка TLS/SSL
+context_ssl = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context_ssl.load_cert_chain(certfile=r'C:\wacs\crt\vm5043127.43ssd.had.wf-crt.pem',
+                            keyfile=r'C:\wacs\crt\vm5043127.43ssd.had.wf-key.pem')
 
 while True:
-    message = server_socket.recv()
-    token = message.decode('utf-8')
-    if check_token(token):
-        print(f"Received request: {token}")
-        server_socket.send(b"World")
-    else:
-        server_socket.send(b"Error token")
+    # Ожидаем следующий запрос от клиента
+    message = socket.recv()
+    print("Received request:", message)
+    time.sleep(1)
+    socket.send(b"World from %s" % port)
