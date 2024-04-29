c92d85f12545c84ad427917220ee3aeef77196b6
gelono <gelonooz@gmail.com>
2024-04-29 13:38:09 +0300

    socket codec

--- sock/skt_server.py
+++ sock/skt_server.py
@@ -1,5 +1,9 @@
 import codecs
+import logging
 import socket
+
+logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+
 
 # Создаем сокет
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,37 +21,29 @@ print("server start...")
 
 while True:
     # Принимаем входящее соединение
-    print('1')
     client_socket, client_address = server_socket.accept()
-    print('2')
     # Выводим информацию о клиенте
     # print("Подключение от клиента:", client_address)
     print(codecs.encode(client_address[0], 'utf-8').decode())
     # ip_address, port_number = client_address
-    # print(f"Подключение от клиента: {ip_address}:{port_number}")
-    print('3')
+    # print(f"Подключение от клиента: {ip_address}:{port_number}"
     # Проверяем IP адрес клиента
     # if client_address[0] == '77.120.144.152':  # Пример проверки IP адреса
-    if client_address[0] == '127.0.0.1':  # Пример проверки IP адреса
-        print('4')
-        print("IP is correct")
+    if client_address[0] == '46.211.228.217':  # Пример проверки IP адреса
+        data = client_socket.recv(1024)
+        if data:
+            msg_client = data.decode()
+            logging.info(f"IP: {msg_client}")
+
         client_socket.sendall(b"IP is correct")
     else:
-        print('5')
         print("IP is failed")
         client_socket.sendall(b"IP is failed")
         client_socket.close()
         continue
 
     # Принимаем данные от клиента и отправляем ответ
-    data = client_socket.recv(1024)
-    print('6')
-    if data:
-        print('7')
-        print("MSG from client:", data.decode())
-        client_socket.sendall(b"Msg recived")
-        print('8')
+
 
     # Закрываем соединение с клиентом
     client_socket.close()
-    print('9')
